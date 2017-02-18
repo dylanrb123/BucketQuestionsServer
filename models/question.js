3ce74cf794a4b82229559563d1d26295a3429ad0
Dylan Bannon <drb2857@rit.edu>
2017-02-17 20:38:49 -0500

    added crud stuff for questions

--- models/question.js
+++ models/question.js
@@ -1,3 +1,13 @@
 /**
  * Created by Dylan on 2/16/2017.
  */
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var QuestionSchema = new Schema({
+    bucket_id: String,
+    text: String,
+    owner: Number
+});
+
+module.exports = mongoose.model('Question', QuestionSchema);
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var Bucket = require('./models/bucket');
+var Question = require('./models/question');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -28,16 +29,14 @@ router.get('/', function (req, res) {
 // Bucket endpoints
 router.route('/buckets')
     .post(function(req, res) {
-    var bucket = new Bucket();
-    console.log(req.body);
-    bucket.name = req.body.name;
-
-    bucket.save(function(err) {
-        if (err) {
-            res.send(err);
-        }
-        res.json({ message: 'Bucket created!'} );
-    })
+        var bucket = new Bucket();
+        bucket.name = req.body.name;
+        bucket.save(function(err) {
+            if (err) {
+                res.send(err);
+            }
+            res.json({ message: 'Bucket created!'} );
+        })
     })
     .get(function(req, res) {
         Bucket.find(function(err, buckets) {
@@ -54,7 +53,6 @@ router.route('/buckets/:bucket_id')
             if (err) {
                 res.send(err);
             }
-
             res.json(bucket);
         })
     })
@@ -81,6 +79,74 @@ router.route('/buckets/:bucket_id')
                 res.send(err);
             }
             res.json({ message: 'Successfully deleted bucket' })
+        })
+    });
+
+router.route('/questions')
+    .post(function (req, res) {
+        var question = new Question();
+        question.bucket_id = req.body.bucket_id;
+        question.text = req.body.text;
+        question.owner = req.body.owner;
+        question.save(function (err) {
+            if (err) {
+                res.send(err);
+            }
+            res.json("Message created!")
+        })
+    })
+    .get(function (req, res) {
+        Question.find(function (err, buckets) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(buckets)
+        })
+    });
+
+router.route('/questions/:question_id')
+    .get(function (req, res) {
+        Question.findById(req.params.question_id, function (err, question) {
+            if (err) {
+                res.send(err);
+            }
+            res.json(question)
+        })
+    })
+    .put(function (req, res) {
+        Question.findById(req.params.question_id, function(err, question) {
+            if (err) {
+                res.send(err);
+            }
+            // only the owner should be able to change the question
+            if (question.owner.toString() === req.body.owner) {
+                console.log('owner matches');
+                question.text = req.body.text;
+            }
+            question.save(function(err) {
+                if (err) {
+                    res.send(err)
+                }
+                res.json("Updated question text")
+            })
+        })
+    })
+    .delete(function (req, res) {
+        Question.findById(req.params.question_id, function(err, question) {
+            if (err) {
+                res.send(err);
+            }
+            // only the owner should be able to delete questions
+            if (question.owner.toString() === req.body.owner) {
+                Question.remove({
+                    _id: req.params.question_id
+                }, function (err, question) {
+                    if (err) {
+                        res.send(err);
+                    }
+                    res.json("Successfully deleted question")
+                })
+            }
         })
     });
 
